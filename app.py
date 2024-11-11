3c97a2fd8c2135922c8c73043302cb605cfe162a
Julian-program <henryjulian.uce@gmail.com>
2024-11-11 00:20:28 -0500

    Modify the message to be displayed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Definir una ruta que responde con "Hola Mundo"
 @app.route("/")
 def hola_mundo():
-    return "Hola Mundo desde una aplicación web en Flask"
+    return "Hola Mundo desde Python"
 
 # Iniciar la aplicación
 if __name__ == "__main__":
